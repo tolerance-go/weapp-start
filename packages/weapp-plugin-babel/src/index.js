b356a9ae40957ef94148bfb91396cefffaf2b72d
bzone <yarnbCoder@gmail.com>
2018-05-07 09:36:58 +0800

    fix: 修复 babel 插件对工作目录 .babelrc 不加载 transform -> transformFileSync

--- packages/weapp-plugin-babel/src/index.js
+++ packages/weapp-plugin-babel/src/index.js
@@ -2,7 +2,7 @@ const babel = require('babel-core');
 
 const babeltrans = ({ config, file, status, extra }, plgConfig) => {
   if (file.extname !== '.js') return;
-  file.contents = babel.transform(file.contents, plgConfig).code;
+  file.contents = babel.transformFileSync(file.path, plgConfig).code;
 };
 
 export default babeltrans;
