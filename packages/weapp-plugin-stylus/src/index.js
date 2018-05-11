5c713f7bde140293b8406e80dcf4df932bdfa45a
bzone <yarnbCoder@gmail.com>
2018-05-11 14:57:12 +0800

    feat: 增加配置参数 兼容 weapp-start 1.0.0

--- packages/weapp-plugin-stylus/src/index.js
+++ packages/weapp-plugin-stylus/src/index.js
@@ -1,15 +1,26 @@
 import stylus from 'stylus';
 
 export default function({ config, file, status, extra }, plgConfig) {
-  if (file.extname !== '.wxss') return;
+  const defaultConfig = {
+    ext: '.wxss',
+    afterExt: '.wxss',
+    filename: file.path,
+    ...plgConfig,
+  };
+
+  // eslint-disable-next-line
+  const { ext, afterExt, ...passConfig } = defaultConfig;
+
+  if (!file.ext.match(defaultConfig.ext)) return;
   let contents = file.contents;
   if (Buffer.isBuffer(file.contents)) {
     contents = file.contents.toString();
   }
   return new Promise((resolve, reject) => {
-    stylus.render(contents, plgConfig, function(err, css) {
+    stylus.render(contents, passConfig, function(err, css) {
       if (err) reject(err);
       file.contents = css;
+      file.ext = defaultConfig.afterExt;
       resolve({ config, file, status, extra });
     });
   });
