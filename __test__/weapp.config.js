054b9fa2dcd504b7525e27cad8bc9ed25c8be54c
bzone <yarnbCoder@gmail.com>
2018-05-11 14:46:46 +0800

    test: 增加测试用例

--- __test__/weapp.config.js
+++ __test__/weapp.config.js
@@ -3,6 +3,12 @@ module.exports = {
     'weapp-plugin-babel',
     'weapp-plugin-require',
     'weapp-plugin-less',
+    [
+      'weapp-plugin-stylus',
+      {
+        ext: '.styl',
+      },
+    ],
     'weapp-plugin-pug',
     'weapp-plugin-eslint',
   ],
