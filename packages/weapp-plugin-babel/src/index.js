d16012777bcbf635360b21e17c0b633455261d01
bzone <yarnbCoder@gmail.com>
2018-05-17 15:18:15 +0800

    feat: 依赖编译

--- packages/weapp-plugin-babel/src/index.js
+++ packages/weapp-plugin-babel/src/index.js
@@ -1,4 +1,14 @@
 const babel = require('babel-core');
+const resolveCwd = require('resolve-cwd');
+const { join, parse } = require('path');
+
+const addExt = (lib, ext = '.js') => {
+  if (lib.match(new RegExp(`${ext}$`))) {
+    return lib;
+  }
+  return lib + ext;
+};
+
 const babeltrans = ({ config, file, status, extra, byDependPaths }, plgConfig) => {
   const defaultConfig = {
     match: /\.js$/,
@@ -11,9 +21,36 @@ const babeltrans = ({ config, file, status, extra, byDependPaths }, plgConfig) =
 
   if (!file.path.match(defaultConfig.match)) return;
 
-const babeltrans = ({ config, file, status, extra }, plgConfig) => {
-  if (file.ext !== '.js') return;
   file.contents = babel.transformFileSync(file.path, plgConfig).code;
+
+  file.contents.replace(/[^.]?require\(['"]([\w\d_\-./@]+)['"]\)/gi, (match, lib) => {
+    let byDependPath;
+
+    if (lib.startsWith('.')) {
+      byDependPath = addExt(join(parse(file.path).dir, lib));
+    }
+
+    if (
+      lib.indexOf('/') === -1 // require('asset');
+    ) {
+      try {
+        resolveCwd(lib);
+      } catch (error) {
+        // var common = require('common.js')
+        // 小程序里面 相对路径可以省略 ./ 直接引用和第三方模块引入方式冲突
+        if (error.code === 'MODULE_NOT_FOUND') {
+          byDependPath = addExt(join(parse(file.path).dir, lib));
+        }
+      }
+    }
+
+    if (byDependPath) {
+      byDependPaths.push({
+        byDependPath,
+        dependPath: file.path,
+      });
+    }
+  });
 };
 
 export default babeltrans;
