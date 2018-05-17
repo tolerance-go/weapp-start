b2598d204db64b8e64c5418c857822159a65408c
bzone <yarnbCoder@gmail.com>
2018-05-17 15:16:46 +0800

    feat: babel add options.match ignore

--- packages/weapp-plugin-babel/src/index.js
+++ packages/weapp-plugin-babel/src/index.js
@@ -1,4 +1,15 @@
 const babel = require('babel-core');
+const babeltrans = ({ config, file, status, extra, byDependPaths }, plgConfig) => {
+  const defaultConfig = {
+    match: /\.js$/,
+    ...plgConfig,
+  };
+
+  if (defaultConfig.ignore) {
+    if (file.path.match(defaultConfig.ignore)) return;
+  }
+
+  if (!file.path.match(defaultConfig.match)) return;
 
 const babeltrans = ({ config, file, status, extra }, plgConfig) => {
   if (file.ext !== '.js') return;
