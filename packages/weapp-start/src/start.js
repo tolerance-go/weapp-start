1a9b1afcf1829a1d069f6fca2c9fb8318608a89d
bzone <yarnbCoder@gmail.com>
2018-05-17 15:29:14 +0800

    feat: 增加插件依赖编译的能力

--- packages/weapp-start/src/start.js
+++ packages/weapp-start/src/start.js
@@ -9,6 +9,26 @@ import resolveCwd from 'resolve-cwd';
 
 const cwd = process.cwd();
 
+// key 被依赖文件，value 依赖文件数组
+const byDependPaths = {
+  _byDependPaths: {},
+  push({ dependPath, byDependPath }) {
+    let depends = this._byDependPaths[byDependPath];
+
+    if (!depends) {
+      this._byDependPaths[byDependPath] = [dependPath];
+      return;
+    }
+
+    if (depends.indexOf(dependPath) !== -1) return;
+
+    depends.push(dependPath);
+  },
+  getDepends(byDependPath) {
+    return this._byDependPaths[byDependPath] || [];
+  },
+};
+
 const getConfig = () => {
   const pts = [join(cwd, 'weapp.config.js'), join(cwd, 'weapp-config.js')];
   let existPt;
@@ -46,7 +66,7 @@ function transform(opts = {}) {
     },
   };
 
-  const options = Promise.resolve({ config, file, status, extra });
+  const options = Promise.resolve({ config, file, status, extra, byDependPaths });
 
   const waitPlg = config.resolvedPlugins.reduce(async (options, next) => {
     return options
