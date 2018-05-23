36ded9e8632c2b0f617517d93e94c2a3afd0b444
bzone <yarnbCoder@gmail.com>
2018-05-23 14:56:24 +0800

    build: 修改发布脚本，支持选择包

--- scripts/publish.js
+++ scripts/publish.js
@@ -27,26 +27,55 @@ if (buildCode === 1) {
   process.exit(1);
 }
 
-const cp = fork(
-  join(process.cwd(), 'node_modules/.bin/lerna'),
-  ['publish', '--skip-npm', '--conventional-commits'].concat(process.argv.slice(2)),
+const prompt = require('inquirer').createPromptModule();
+prompt([
   {
-    stdio: 'inherit',
-    cwd: process.cwd(),
-  }
-);
-cp.on('error', err => {
-  console.log(err);
-});
-cp.on('close', code => {
-  console.log('code', code);
-  if (code === 1) {
-    console.error('Failed: lerna publish');
-    process.exit(1);
-  }
+    name: 'package',
+    type: 'checkbox',
+    message: '选择发布的包',
+    choices: updatedRepos,
+  },
+])
+  .then(input => {
+    if (!input.package.length) {
+      return console.log('no select npm prepublish!');
+    }
 
-  publishToNpm();
-});
+    const ignores = updatedRepos.filter(item => !input.package.includes(item));
+
+    const cp = fork(
+      join(process.cwd(), 'node_modules/.bin/lerna'),
+      ignores
+        .reduce(
+          (a, b) => {
+            a.push('--ignore');
+            a.push(b);
+            return a;
+          },
+          ['publish', '--skip-npm', '--conventional-commits']
+        )
+        .concat(process.argv.slice(2)),
+      {
+        stdio: 'inherit',
+        cwd: process.cwd(),
+      }
+    );
+    cp.on('error', err => {
+      console.log(err);
+    });
+    cp.on('close', code => {
+      console.log('code', code);
+      if (code === 1) {
+        console.error('Failed: lerna publish');
+        publishToNpm();
+        process.exit(1);
+      }
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    process.exit(1);
+  });
 
 function publishToNpm() {
   console.log(`repos to publish: ${updatedRepos.join(', ')}`);
