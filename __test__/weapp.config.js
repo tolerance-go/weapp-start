9541e0f4d9b37e85e17eee5a09ff28d8039f3643
bzone <yarnbCoder@gmail.com>
2018-05-31 22:10:31 +0800

    test: add test

--- __test__/weapp.config.js
+++ __test__/weapp.config.js
@@ -1,39 +1,52 @@
 module.exports = {
   plugins: [
-    'weapp-plugin-babel',
-    'weapp-plugin-require',
-    'weapp-plugin-less',
+    [
+      'weapp-plugin-replace',
+      {
+        match: /\.wxss$/,
+        config: {
+          find: /([0-9])px/gi,
+          replace: (match, count) => {
+            console.log(match, count);
+            return count + 'rpx';
+          },
+        },
+      },
+    ],
+    // 'weapp-plugin-babel',
+    // 'weapp-plugin-require',
+    // 'weapp-plugin-less',
+    // // [
+    // //   'weapp-plugin-filter',
+    // //   {
+    // //     throw: /\.styl$/,
+    // //   },
+    // // ],
     // [
-    //   'weapp-plugin-filter',
+    //   'weapp-plugin-stylus',
     //   {
-    //     throw: /\.styl$/,
+    //     match: /\.styl$/,
     //   },
     // ],
-    [
-      'weapp-plugin-stylus',
-      {
-        match: /\.styl$/,
-      },
-    ],
-    'weapp-plugin-pug',
-    'weapp-plugin-eslint',
-    [
-      'weapp-plugin-jsmin',
-      {
-        // extra: true,
-      },
-    ],
-    'weapp-plugin-imgmin',
-    // 'weapp-plugin-filemin',
-    [
-      'weapp-plugin-postcss',
-      {
-        plugins: [
-          require('autoprefixer')({
-            browsers: ['Android >= 2.3', 'Chrome > 20', 'iOS >= 6'],
-          }),
-        ],
-      },
-    ],
+    // 'weapp-plugin-pug',
+    // 'weapp-plugin-eslint',
+    // [
+    //   'weapp-plugin-jsmin',
+    //   {
+    //     // extra: true,
+    //   },
+    // ],
+    // 'weapp-plugin-imgmin',
+    // // 'weapp-plugin-filemin',
+    // [
+    //   'weapp-plugin-postcss',
+    //   {
+    //     plugins: [
+    //       require('autoprefixer')({
+    //         browsers: ['Android >= 2.3', 'Chrome > 20', 'iOS >= 6'],
+    //       }),
+    //     ],
+    //   },
+    // ],
   ],
 };
